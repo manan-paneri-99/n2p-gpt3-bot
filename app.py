360fac773259a27c0edfa2c2ecdfc3df24c017e9
Manan Paneri <39223931+manan-paneri-99@users.noreply.github.com>
2021-05-03 23:11:18 +0530

    add: app.py and n2pbot.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, session
+from twilio.twiml.messaging_response import MessagingResponse
+from n2pbot import convert, append_interaction_to_chat_log
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'kEy@secReT_notes-to-points'
+
+@app.route('/n2pbot', methods = ['POST'])
+def n2pbot():
+	incoming_msg = request.values['Body']
+	chat_log = session.get('chat_log')
+
+	point = convert(incoming_msg, chat_log)
+	session['chat_log'] = append_interaction_to_chat_log(incoming_msg, point,
+                                                         chat_log)
+	r = MessagingResponse()
+	r.message(point)
+	return str(r)
